96fb90f5af455bce72680a8c6c91a25bcb4d4f81
DawnKiro <kirobyte4@gmail.com>
2024-10-26 18:58:13 -0500

    Update PlayerEntityAnimator.java

--- src/main/java/net/voxelden/radiationApocalypse/client/render/PlayerEntityAnimator.java
+++ src/main/java/net/voxelden/radiationApocalypse/client/render/PlayerEntityAnimator.java
@@ -5,20 +5,20 @@ import software.bernie.geckolib.animation.*;
 
 public class PlayerEntityAnimator {
     public static void registerControllers(PlayerEntity player, AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<>(player, "base", 5, event -> event.setAndContinue(PlayerAnimation.BASE)));
-        controllers.add(new AnimationController<>(player, "movement", 5, PlayerEntityAnimator::movementAnimationController));
+        controllers.add(new AnimationController<>(player, "base", 20, event -> event.setAndContinue(PlayerAnimation.BASE)));
+        controllers.add(new AnimationController<>(player, "movement", 20, PlayerEntityAnimator::movementAnimationController));
     }
 
     protected static PlayState movementAnimationController(final AnimationState<PlayerEntity> event) {
-        if (event.isMoving()) {}
+        if (event.isMoving())
+            return event.setAndContinue(PlayerAnimation.MOVING);
         else
             return event.setAndContinue(PlayerAnimation.IDLE);
-
-        return PlayState.STOP;
     }
 
     public static class PlayerAnimation {
         public static final RawAnimation BASE = RawAnimation.begin().thenLoop("base");
         public static final RawAnimation IDLE = RawAnimation.begin().thenLoop("idle");
+        public static final RawAnimation MOVING = RawAnimation.begin().thenLoop("moving");
     }
 }
